5d0bcddb8fed6ef109efd439f6db79f1b9ae2354
Rafael de Jesus Silva Monteiro <62445262+rafaeldjsm@users.noreply.github.com>
2021-09-16 09:43:55 -0300

    Add files via upload

---
+++ Turtle/turtle_circle.py
@@ -0,0 +1,19 @@
+﻿import math
+import turtle
+
+def drawpolig(t, r, n):
+    """Make turtle t draw a circle of radius r."""
+    
+    alfa_r = 2*math.pi/n
+    alfa_d = 360/n
+
+    for i in range(n):
+        fw = 2*r*math.sin(alfa_r/2)
+        t.forward(fw)
+        t.right(alfa_d)
+        
+print(math.pi)
+
+t = turtle.Turtle()
+
+drawpolig(t, 100, 30)
