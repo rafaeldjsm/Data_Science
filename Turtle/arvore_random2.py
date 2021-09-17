16b967aac3886a18e40356d57373216eae793385
Rafael de Jesus Silva Monteiro <62445262+rafaeldjsm@users.noreply.github.com>
2021-09-17 14:01:57 -0300

    Add files via upload

---
+++ Turtle/arvore_random2.py
@@ -0,0 +1,39 @@
+﻿import turtle
+import random
+
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        
+        ang = random.randint(10,30)
+        dec = random.randint(12,18)
+        
+        t.right(ang)
+        t.forward(branchLen-dec)
+        tree(branchLen-dec,t)
+        t.backward(branchLen-dec)
+        t.left(2*ang)
+        t.forward(branchLen-dec)
+        tree(branchLen-dec,t)
+        t.backward(branchLen-dec)
+        t.right(ang)
+        tree(branchLen-dec,t)
+        t.forward(-branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    myWin.tracer(100)
+    t.left(90)
+    t.up()
+    t.ht()
+    t.backward(200)
+    t.down()
+    t.color("green")
+    t.right(30)
+    for k in range(1,4):
+        t.left(k*10)
+        tree(65,t)
+    myWin.exitonclick()
+
+main()
