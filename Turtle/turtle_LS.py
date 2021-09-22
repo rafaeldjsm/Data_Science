f3456c993a03634128cf88c494ab71b58fcae0be
Rafael de Jesus Silva Monteiro <62445262+rafaeldjsm@users.noreply.github.com>
2021-09-22 08:20:54 -0300

    Add files via upload

---
+++ Turtle/turtle_LS.py
@@ -0,0 +1,56 @@
+import turtle
+
+def turtlecmd(t,turtle_program, turn_amount=45):
+    saved_states = []
+    head_states = []
+    for command in turtle_program:
+        if command in "ABCDEFGHIJ":
+            t.down()
+            t.fd(1)
+        elif command == "abcdefghij":
+            t.up()
+            t.fd(1)
+        elif command == '+':     # Turn turtle clockwise without moving
+            t.down()
+            t.left(turn_amount)
+            
+        elif command == '-':     # Turn turtle counter-clockwise without moving
+            t.down()
+            t.right(turn_amount)
+            
+        elif command == '[':                       # Remember current state
+            saved_states.append(t.pos())
+            head_states.append(t.heading())
+
+        elif command == ']':                       # Return to previous state
+            state = saved_states.pop()
+            t.goto(state)
+            t.setheading(head_states.pop())
+            
+def transform_sequence(sequence, transformations):
+    return ''.join(transformations.get(c, c) for c in sequence)
+
+
+def transform_multiple(sequence, transformations, iterations):
+    for _ in range(iterations):
+        sequence = transform_sequence(sequence, transformations)
+    return sequence
+
+t = turtle.Turtle()
+wn = turtle.Screen()
+
+t.speed("fastest")
+wn.setworldcoordinates(-50,0,50,100)
+wn.tracer(100)
+
+t.left(90)
+#turtlecmd(t,transform_multiple('F', {'F': '+F+F--F+F'}, 5))
+
+t.speed("fastest")
+
+#turtlecmd(t,transform_multiple('F', {'F': 'FF[++F][-FF]'}, 5),22)
+
+turtlecmd(t,transform_multiple('A', {'F': 'FF', 'A': 'F[+AF-[A]--A][---A]'}, 5),22.5)
+
+
+
